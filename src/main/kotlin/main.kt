4a9c32888089e68108aac622469992407a71be52
Shifterok <Lokky123@mail.ru>
2021-10-17 21:24:35 +0300

    HM2.3 done

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,35 @@
-fun main(){
+fun main() {
     println("Наш пользователь каждый месяц покупает пластинки? (да/нет)")
     val areHeSheMusicLover = readLine()
-
-    val discount = true
-    when(areHeSheMusicLover == "да"){
-        false -> val discountPrice = 10
-        true -> val discountPrice = 1.01
+    //Определение "меломана"
+    val discountPrice = when (areHeSheMusicLover == "да") {
+        false -> 0
+        true -> 1
     }
-    println ("Ваша сикдка - ${discountPrice}")
+    println("Ваша сикдка гарантированная скидка - $discountPrice %")
+    println("Сколько денег вы потратили у нас ранее? (укажите в рублях)")
+    val pastBuy = readLine()!!.toInt()
+    println("На какую суму вы покупаете песню (укажите в рублях)")
+    val presentBuy = readLine()!!.toInt()
+
+    when {
+        areHeSheMusicLover == "да" -> {
+            if (pastBuy in 0..1_000) {
+                println("Вы купили мелодию за  ${presentBuy * 0.99}")
+            } else if (pastBuy in 1_001..10_000) {
+                println("Вы купили мелодию за  ${presentBuy * 0.99 - 100}")
+            } else if (pastBuy in 10_001..1_000_000) {
+                println("Вы купили мелодию за  ${presentBuy - (presentBuy * 0.99 - presentBuy * 0.95)}")
+            }
+        }
+        areHeSheMusicLover == "нет" -> {
+            if (pastBuy in 0..1_000) {
+                println("Вы купили мелодию за  $presentBuy")
+            } else if (pastBuy in 1_001..10_000) {
+                println("Вы купили мелодию за  ${presentBuy - 100}")
+            } else if (pastBuy in 10_001..1_000_000) {
+                println("Вы купили мелодию за  ${presentBuy - presentBuy * 0.95}")
+            }
+        }
+    }
 }
