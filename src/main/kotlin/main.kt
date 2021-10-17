b6bb5830927fa8d37a37cc86ddaa326047c8a818
Shifterok <Lokky123@mail.ru>
2021-10-17 18:00:11 +0300

    commit 1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main(){
-    
+    println("Наш пользователь каждый месяц покупает пластинки? (да/нет)")
+    val areHeSheMusicLover = readLine()
+
+    val discount = true
+    when(areHeSheMusicLover == "да"){
+        false -> val discountPrice = 10
+        true -> val discountPrice = 1.01
+    }
+    println ("Ваша сикдка - ${discountPrice}")
 }
