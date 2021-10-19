ffb48951807d3fd9831c0d53047e005233f4e9a7
Shifterok <Lokky123@mail.ru>
2021-10-19 20:49:38 +0300

    remarks done

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,23 +12,31 @@ fun main() {
     println("На какую суму вы покупаете песню (укажите в рублях)")
     val presentBuy = readLine()!!.toInt()
 
-    when {
-        areHeSheMusicLover == "да" -> {
-            if (pastBuy in 0..1_000) {
-                println("Вы купили мелодию за  ${presentBuy * 0.99}")
-            } else if (pastBuy in 1_001..10_000) {
-                println("Вы купили мелодию за  ${presentBuy * 0.99 - 100}")
-            } else if (pastBuy in 10_001..1_000_000) {
-                println("Вы купили мелодию за  ${presentBuy - (presentBuy * 0.99 - presentBuy * 0.95)}")
+    when (areHeSheMusicLover) {
+        "да" -> {
+            when (pastBuy) {
+                in 0..1_000 -> {
+                    println("Вы купили мелодию за  ${presentBuy * 0.99}")
+                }
+                in 1_001..10_000 -> {
+                    println("Вы купили мелодию за  ${presentBuy * 0.99 - 100}")
+                }
+                in 10_001..1_000_000 -> {
+                    println("Вы купили мелодию за  ${presentBuy - (presentBuy * 0.99 - presentBuy * 0.95)}")
+                }
             }
         }
-        areHeSheMusicLover == "нет" -> {
-            if (pastBuy in 0..1_000) {
-                println("Вы купили мелодию за  $presentBuy")
-            } else if (pastBuy in 1_001..10_000) {
-                println("Вы купили мелодию за  ${presentBuy - 100}")
-            } else if (pastBuy in 10_001..1_000_000) {
-                println("Вы купили мелодию за  ${presentBuy - presentBuy * 0.95}")
+        "нет" -> {
+            when (pastBuy) {
+                in 0..1_000 -> {
+                    println("Вы купили мелодию за  $presentBuy")
+                }
+                in 1_001..10_000 -> {
+                    println("Вы купили мелодию за  ${presentBuy - 100}")
+                }
+                in 10_001..1_000_000 -> {
+                    println("Вы купили мелодию за  ${presentBuy - presentBuy * 0.95}")
+                }
             }
         }
     }
